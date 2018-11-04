c8358084440cefc4a60210486e9713ac1e8d8f6b
Chris Woodle <chris@droplit.io>
2018-11-03 23:22:07 -0400

    search with style

--- src/search.vue
+++ src/search.vue
@@ -1,14 +1,23 @@
 <template>
     <div>
-        <div class='uk-container'>
-            <div class="uk-section">
-                <h1>Search!</h1>
-                <vue-fuse :keys="keys" :list="bikes" event-name="results" placeholder="Search for bikes"></vue-fuse>
-                <div v-for="bike in results" :key="bike.model.name + bike.model.id">
-                    {{bike}}
-                </div>
+        <div class="uk-section">
+            <div class='uk-container'>
+                <h1>Search</h1>
+                <vue-fuse :keys="keys" :list="entries" event-name="results" placeholder="Search for data"></vue-fuse>
             </div>
-
+        </div>
+        <div class="uk-section uk-section-muted">
+            <div class='uk-container'>
+                <vk-card v-for="entry in results" :key="entry.url" class="uk-margin-bottom">
+                    <vk-card-title>{{entry.title}}</vk-card-title>
+                    <p>{{entry.description}}</p>
+                    <vk-label type="primary" v-for="type in entry.type" :key="type">{{type}}</vk-label>
+                    <vk-label type="success" v-for="tag in entry.tags" :key="tag">{{tag}}</vk-label>
+                    <div slot="footer">
+                        <vk-button-link type="text" :href="entry.url">Go to data</vk-button-link>
+                    </div>
+                </vk-card>
+            </div>
         </div>
     </div>
 </template>
@@ -37,13 +46,15 @@ export default Vue.extend({
     data: function (this: any) {
         ax.get('data.json').then(response => {
             console.log(response);
-            this.bikes = response.data.data;
+            this.categories = response.data.categories;
+            this.entries = response.data.entries;
         })
 
         return {
             results: [],
-            bikes: [],
-            keys: ["brand", "model.name", "model.id"]
+            categories: [],
+            entries: [],
+            keys: ["title", "source", "type", "tags"]
         };
     }
 });
@@ -51,7 +62,4 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-</style>
-
-<style lang="scss">
 </style>
